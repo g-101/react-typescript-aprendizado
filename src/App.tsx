5a05ec66bea286b6cba6234994df132f569a88e7
genesis-g101 <gyreplicant@gmail.com>
2022-10-19 14:51:56 -0300

    usando o useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
-// import './App.css';
-
-import { Button } from './components/Button/Button';
+import './App.css';
+import { ChangeEvent, useEffect, useState } from 'react';
 
 function App() {
+  const [name, setName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [fullName, setFullName] = useState('');
+
+  useEffect(() => {
+    // alert('executou');
+    setFullName(`${name}  ${lastName}`);
+  }, [name, lastName]);
+  /*o segundo parametro, no array, são onde fica os observers */
+  const nameChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  };
+  const lastNameChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setLastName(event.target.value);
+  };
   return (
     <div className="App">
-      <Button />
+      <input type="text" placeholder="DIGITE O NOME" value={name} onChange={nameChangeHandler} />
+      <input
+        type="text"
+        placeholder="DIGITE O SOBRENOME"
+        value={lastName}
+        onChange={lastNameChangeHandler}
+      />
+      <p>Nome completo: {fullName}</p>
     </div>
   );
 }
