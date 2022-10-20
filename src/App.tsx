299cf8b4fd8172d0a53de29daf433fc024a40979
genesis-g101 <gyreplicant@gmail.com>
2022-10-20 09:37:47 -0300

    lidando com o loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,41 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import { MovieTypes } from './types/MovieTypes';
 import { Item } from './components/Item';
 
 function App() {
   const [movies, setMovies] = useState<MovieTypes[]>([]);
-  // const loadMoviesHandler = () => {
-  //   fetch('https://api.b7web.com.br/cinema/')
-  //     .then(res => {
-  //       return res.json();
-  //     })
-  //     .then(json => {
-  //       setMovies(json);
-  //     });
-  // };
+  const [loading, setLoading] = useState(false);
+
   const loadMoviesHandler = async () => {
     try {
+      setLoading(true);
       const res = await fetch('https://api.b7web.com.br/cinema/'); /* retorna a resposta no fetch */
       const json = await res.json(); /* transforma em json */
+      setLoading(false);
       setMovies(json); /*atualiza o state com o json */
     } catch (err) {
       console.log(err);
     }
   };
 
+  useEffect(() => {
+    loadMoviesHandler();
+  }, []);
+
   return (
     <div className="wrapper">
-      <p>Total de filmes: {movies.length}</p>
-      <ol className="list">
-        {movies.map((movie, index) => (
-          <Item key={index} data={movie} />
-        ))}
-      </ol>
-      <button className="btn" onClick={loadMoviesHandler}>
-        Carregar filmes
-      </button>
+      {loading && <p className="title">Carregando...</p>}
+      {!loading && (
+        <>
+          <p className="title">Total de filmes: {movies.length}</p>
+          <ol className="list">
+            {movies.map((movie, index) => (
+              <Item key={index} data={movie} />
+            ))}
+          </ol>
+        </>
+      )}
     </div>
   );
 }
