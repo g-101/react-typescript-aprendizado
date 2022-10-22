0fd72a1a46057d3757723fa813d21852674705bb
genesis-g101 <gyreplicant@gmail.com>
2022-10-22 13:19:13 -0300

    intro ao useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,39 @@
-import React, { useEffect, useState } from 'react';
+import React, { useReducer } from 'react';
 import './App.css';
-import { PostForm } from './components/PostForm';
-import { PostItem } from './components/PostItem';
-import { PostTypes } from './types/PostTypes';
-import { api } from './api';
-
-function App() {
-  const [posts, setPosts] = useState<PostTypes[]>([]);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    loadPostHandler();
-  }, []);
-
-  const loadPostHandler = async () => {
-    try {
-      setLoading(true);
-      const answer = await api.getAllPosts();
-      setLoading(false);
-      setPosts(answer);
-    } catch (error) {
-      setLoading(false);
-      alert('ocorreu um erro, tente novamente mais tarde');
-      setPosts([]);
-      console.error(error);
-    }
-  };
-  const createPostHandler = async (title: string, body: string) => {
-    try {
-      await api.createNewPost(1, title, body);
-      alert('Post adicionado com sucesso');
-    } catch (error) {
-      alert('ocorreu um erro');
-
-      console.error(error);
-    }
-  };
-
+type reducerState = {
+  count: number;
+};
+type actionState = {
+  type: string;
+};
+const initialState = { count: 0 };
+const reducer = (state: reducerState, action: actionState) => {
+  switch (action.type) {
+    case '+':
+      // não modificar o objeto state diretamente para manter a imutabilidade no código
+      return { ...state, count: state.count + 1 };
+    case '-':
+      if (state.count > 0) {
+        return { ...state, count: state.count - 1 };
+      }
+      break;
+    case 'reset':
+      return initialState;
+  }
+  return state;
+};
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
     <div className="wrapper">
-      {loading && <p className="title">Carregando...</p>}
-      <PostForm onAdd={createPostHandler} />
-
-      {!loading && posts.length > 0 && (
-        <>
-          <p className="title">Total de posts: {posts.length}</p>
-
-          {posts.map((item, index) => (
-            <PostItem dataPost={item} key={index} />
-          ))}
-        </>
-      )}
-      {!loading && posts.length === 0 && <p className="title">Não há post para exibir</p>}
+      <p>Contagem: {state.count}</p>
+      <div>
+        <button onClick={() => dispatch({ type: '+' })}>+</button>
+        <button onClick={() => dispatch({ type: '-' })}>-</button>
+        <button onClick={() => dispatch({ type: 'reset' })}>reset</button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
