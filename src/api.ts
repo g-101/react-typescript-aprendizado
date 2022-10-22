81967cc878f4b4b37d56e6092d4561ff1ec3eae4
genesis-g101 <gyreplicant@gmail.com>
2022-10-22 10:41:02 -0300

    adicionando o axios

--- src/api.ts
+++ src/api.ts
@@ -1,24 +1,34 @@
-const BASE = 'https://jsonplaceholder.typicode.com';
+import axios from 'axios';
+
+const http = axios.create({
+  baseURL: 'https://jsonplaceholder.typicode.com',
+  headers: { 'X-Requested-With': 'XMLHttpRequest' },
+});
 
 export const api = {
   getAllPosts: async () => {
-    const response = await fetch(`${BASE}/posts`);
-    const json = await response.json();
-    return json;
+    try {
+      const response = await http.get('/posts');
+      return response.data;
+    } catch (error) {
+      console.error(error);
+    }
   },
+  // getAllPosts: async () => {
+  //   const response = await fetch(`${BASE}/posts`);
+  //   const json = await response.json();
+  //   return json;
+  // },
   createNewPost: async (userId: number, title: string, body: string) => {
-    const response = await fetch(`${BASE}/posts`, {
-      method: 'POST',
-      body: JSON.stringify({
+    try {
+      const response = await http.post('/posts', {
         userId: userId,
         title: title,
         body: body,
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      }),
-    });
-    const json = await response.json();
-    return json;
+      });
+      return response.data;
+    } catch (error) {
+      console.error(error);
+    }
   },
 };
