d5769980f29b4c10538fe524ae9ce65dd129f6fb
genesis-g101 <gyreplicant@gmail.com>
2022-11-01 10:01:32 -0300

    usando o useReducer na pratica

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,46 @@
-import './App.css';
-import { useCounter } from './reducers/counter';
+import { usePeopleList } from './reducers/peopleList';
 
+import { ChangeEvent, useState } from 'react';
+import './App.css';
 const App = () => {
-  const [state, counterDispatch] = useCounter();
+  const [list, dispatch] = usePeopleList();
+  const [nameInput, setNameInput] = useState('');
+  const addButtonHandler = () => {
+    if (nameInput) {
+      dispatch({
+        type: 'add',
+        payload: {
+          name: nameInput,
+        },
+      });
+      setNameInput('');
+    }
+  };
+  const inputChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setNameInput(event.target.value);
+  };
+  const deleteButtonHandler = (id: string) => {
+    dispatch({
+      type: 'del',
+      payload: {
+        id: id,
+      },
+    });
+  };
   return (
     <div className="wrapper">
-      <p>Contagem: {state.count}</p>
-      <div>
-        <button onClick={() => counterDispatch({ type: '+' })}>+</button>
-        <button onClick={() => counterDispatch({ type: '-' })}>-</button>
-        <button onClick={() => counterDispatch({ type: 'reset' })}>reset</button>
-      </div>
+      <input className="input-text" type="text" value={nameInput} onChange={inputChangeHandler} />
+      <button onClick={addButtonHandler}>Adicionar</button>
+      <hr />
+      <ul>
+        <p>Lista de Pessoas: </p>
+        {list.map((item, index) => (
+          <li key={index}>
+            {item.name}
+            <button onClick={() => deleteButtonHandler(item.id)}>[ deletar ]</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
