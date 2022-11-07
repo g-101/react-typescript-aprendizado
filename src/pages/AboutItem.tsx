d3894d00b36d07e9c24dc95a69bcef966cdb1727
genesis-g101 <gyreplicant@gmail.com>
2022-11-07 10:47:41 -0300

    redirecionando rotas

--- src/pages/AboutItem.tsx
+++ src/pages/AboutItem.tsx
@@ -1,6 +1,20 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 
 export const AboutItem = () => {
   const { slug } = useParams();
-  return <div>Pagina sobre {slug}</div>;
+  const navigate = useNavigate();
+  const backButtonHandler = () => {
+    navigate(-1);
+  };
+  const homeButtonHandler = () => {
+    navigate('/');
+  };
+  return (
+    <div>
+      Pagina sobre {slug}
+      <hr />
+      <button onClick={backButtonHandler}>Voltar</button>
+      <button onClick={homeButtonHandler}>Home</button>
+    </div>
+  );
 };
