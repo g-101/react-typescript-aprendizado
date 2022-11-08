dab539a1bc1b1a4358f3471d83530ec68194ee8b
genesis-g101 <gyreplicant@gmail.com>
2022-11-08 10:54:52 -0300

    parametros query

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,19 @@
-import { Link } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 export const About = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const setOrder = (order: 'asc' | 'desc') => {
+    searchParams.set('order', order);
+    setSearchParams(searchParams);
+  };
   return (
     <div>
+      Filtro: {searchParams.get('filter')}
+      <br />
+      Ordem: {searchParams.get('order')}
+      <hr />
+      <button onClick={() => setOrder('asc')}>Asc</button>
+      <button onClick={() => setOrder('desc')}>Desc</button>
+      <hr />
       Sobre:
       <ul>
         <li>
